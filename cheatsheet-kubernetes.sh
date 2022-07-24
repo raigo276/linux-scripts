fea982143923e88b17bbf40c2106cb6e6d9441ba
raigo276 <rainel.gonzalez@web.de>
2022-07-24 21:32:16 +0200

    Extension changes

---
+++ cheatsheet-kubernetes.sh
@@ -0,0 +1,19 @@
+# Managing Applications with Kubernetes
+# The kubectl CLI
+# Command Description
+kubectl apply # Apply a configuration to a resource.
+kubectl config get-clusters # Displays clusters defined in the kubeconfig.
+kubectl config get-contexts # Displays the current context.
+kubectl create # Create a resource.
+kubectl delete # Deletes resources.
+kubectl describe # Shows details of a resource or group of resources.
+kubectl expose # Expose a resource to the internet as a new Kubernetes service.
+kubectl get # Displays resources.
+kubectl get pods # Lists all the Pods in the namespace.
+kubectl proxy # Creates a proxy server between a localhost and the Kubernetes API
+server.
+kubectl rollout # Manage the rollout of a resource.
+kubectl run # Creates and runs a particular image in a pod.
+kubectl scale # Set a new size for a deployment.
+kubectl set # Configure application resources.
+kubectl version # Prints the client and server version information.
