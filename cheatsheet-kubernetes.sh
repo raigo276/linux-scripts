703905e3aa753132044b8acbe0c860998f088782
raigo276 <rainel.gonzalez@web.de>
2022-07-24 22:12:15 +0200

    new changes

--- cheatsheet-kubernetes.sh
+++ cheatsheet-kubernetes.sh
@@ -10,10 +10,16 @@ kubectl describe # Shows details of a resource or group of resources.
 kubectl expose # Expose a resource to the internet as a new Kubernetes service.
 kubectl get # Displays resources.
 kubectl get pods # Lists all the Pods in the namespace.
-kubectl proxy # Creates a proxy server between a localhost and the Kubernetes API
-server.
+kubectl proxy # Creates a proxy server between a localhost and the Kubernetes API server.
 kubectl rollout # Manage the rollout of a resource.
 kubectl run # Creates and runs a particular image in a pod.
 kubectl scale # Set a new size for a deployment.
 kubectl set # Configure application resources.
 kubectl version # Prints the client and server version information.
+
+
+# Red Hat Openshift cli
+# NOTE: all kubectl can be replaced with oc in openshift
+oc get #Display a resource.
+oc project #Switch to a different project.
+oc version #Display version information.
