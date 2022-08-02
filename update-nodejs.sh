f3cb8af14a1c748266b4c72127e59dd5d05ca43a
raigo276 <rainel.gonzalez@web.de>
2022-08-02 07:49:10 -0700

    Modification install nodejs

--- update-nodejs.sh
+++ update-nodejs.sh
@@ -2,5 +2,3 @@
 sudo apt-get update
 sudo apt-get install build-essential checkinstall libssl-dev -y
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-sudo nvm install node
-node -v
